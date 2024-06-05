7ca2548d3c981383e4fa86636a8f231b8b65ed09
Richard Hague <rhagueweb@gmail.com>
2024-06-06 00:42:51 +0100

    Updated CV and socials

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,62 +1,49 @@
-import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
-import {
-  AiFillGithub,
-  AiFillInstagram,
-} from "react-icons/ai";
-import { FaLinkedinIn } from "react-icons/fa";
-import { FaXTwitter } from "react-icons/fa6";
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import { AiFillGithub } from 'react-icons/ai';
+import { FaLinkedinIn } from 'react-icons/fa';
+import { FaXTwitter } from 'react-icons/fa6';
 
 function Footer() {
   let date = new Date();
   let year = date.getFullYear();
   return (
-    <Container fluid className="footer">
+    <Container fluid className='footer'>
       <Row>
-        <Col md="4" className="footer-copywright">
+        <Col md='4' className='footer-copywright'>
           <h3>Copyright © {year} rhagueweb</h3>
         </Col>
-        <Col md="4" className="footer-copywright"></Col>
-        <Col md="4" className="footer-body">
-          <ul className="footer-icons">
-            <li className="social-icons">
+        <Col md='4' className='footer-copywright'></Col>
+        <Col md='4' className='footer-body'>
+          <ul className='footer-icons'>
+            <li className='social-icons'>
               <a
-                href="https://github.com/rhagueweb"
-                style={{ color: "white" }}
-                target="_blank" 
-                rel="noopener noreferrer"
+                href='https://github.com/rhagueweb'
+                style={{ color: 'white' }}
+                target='_blank'
+                rel='noopener noreferrer'
               >
                 <AiFillGithub />
               </a>
             </li>
-            <li className="social-icons">
+            <li className='social-icons'>
               <a
-                href="https://twitter.com/rhagueweb"
-                style={{ color: "white" }}
-                target="_blank" 
-                rel="noopener noreferrer"
+                href='https://twitter.com/rhagueweb'
+                style={{ color: 'white' }}
+                target='_blank'
+                rel='noopener noreferrer'
               >
                 <FaXTwitter />
               </a>
             </li>
-            <li className="social-icons">
+            <li className='social-icons'>
               <a
-                href="https://www.linkedin.com/in/rhagueweb"
-                style={{ color: "white" }}
-                target="_blank" 
-                rel="noopener noreferrer"
+                href='https://www.linkedin.com/in/rhagueweb'
+                style={{ color: 'white' }}
+                target='_blank'
+                rel='noopener noreferrer'
               >
                 <FaLinkedinIn />
-              </a>
-            </li>
-            <li className="social-icons">
-              <a
-                href="https://www.instagram.com/rhague90"
-                style={{ color: "white" }}
-                target="_blank" 
-                rel="noopener noreferrer"
-              >
-                <AiFillInstagram />
               </a>
             </li>
           </ul>
--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -1,92 +1,94 @@
-import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
-import myImg from "../../Assets/RH_ProfilePic.jpg";
-import {
-  AiFillGithub,
-  AiFillInstagram,
-} from "react-icons/ai";
-import { FaLinkedinIn } from "react-icons/fa";
-import { FaXTwitter } from "react-icons/fa6";
-
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import myImg from '../../Assets/RH_ProfilePic.jpg';
+import { AiFillGithub } from 'react-icons/ai';
+import { FaLinkedinIn } from 'react-icons/fa';
+import { FaXTwitter } from 'react-icons/fa6';
 
 function Home2() {
   return (
-    <Container fluid className="home-about-section" id="about">
+    <Container fluid className='home-about-section' id='about'>
       <Container>
         <Row>
-          <Col md={8} className="home-about-description">
-            <h1 style={{ fontSize: "2.6em" }}>
-              LET ME <span className="purple"> INTRODUCE </span> MYSELF
+          <Col md={8} className='home-about-description'>
+            <h1 style={{ fontSize: '2.6em' }}>
+              LET ME <span className='purple'> INTRODUCE </span> MYSELF
             </h1>
-            <p className="home-about-body">
-              Hey there! I'm Richard Hague (33), hailing from the charming town of <b className="purple">Macclesfield</b> in Cheshire, England—just a stone's throw from the vibrant city of Manchester. 
-              <br /><br />
-              By day, I'm your friendly neighborhood <b className="purple">iMIS</b> consultant, weaving magic with databases. By night, I immerse myself in the endless possibilities of web development, crafting digital marvels in the world of freelance.
-              <br /><br />
-              Beyond the confines of code, you'll find me gazing at the stars, cheering for <b className="purple">Manchester United</b>, or engaging in some fierce <b className="purple">table tennis</b> battles.
-              <br /><br />
-              Join me on this cosmic journey where technology meets the stars, and passion fuels the game!
+            <p className='home-about-body'>
+              Hey there! I'm Richard Hague (33), hailing from the charming town
+              of <b className='purple'>Macclesfield</b> in Cheshire,
+              England—just a stone's throw from the vibrant city of Manchester.
+              <br />
+              <br />
+              By day, I'm your friendly neighborhood{' '}
+              <b className='purple'>iMIS</b> consultant, weaving magic with
+              databases. By night, I'm on a web development journey, learning
+              the MERN stack along with headless CMS's like Strapi and Payload,
+              and exploring Next.js and Tailwind CSS to create beautiful and
+              functional websites.
+              <br />
+              <br />
+              Beyond the confines of code, you'll find me gazing at the stars,
+              cheering for <b className='purple'>Manchester United</b>, or
+              engaging in some fierce <b className='purple'>table tennis</b>{' '}
+              battles.
+              <br />
+              <br />
+              Join me on this cosmic journey where technology meets the stars,
+              and passion fuels the game!
             </p>
           </Col>
-          <Col md={4} className="myAvtar">
-            <div style={{
-              width: 200,
-              height: 200,
-              borderRadius: '50%',
-              overflow: 'hidden',
-              margin: 'auto'
-            }}>
-              <img src={myImg} className="img-fluid" alt="avatar" />
+          <Col md={4} className='myAvtar'>
+            <div
+              style={{
+                width: 200,
+                height: 200,
+                borderRadius: '50%',
+                overflow: 'hidden',
+                margin: 'auto',
+              }}
+            >
+              <img src={myImg} className='img-fluid' alt='avatar' />
             </div>
           </Col>
         </Row>
         <Row>
-          <Col md={12} className="home-about-social">
+          <Col md={12} className='home-about-social'>
             <h1>FIND ME ON</h1>
-            <ul className="home-about-social-links">
-              <li className="social-icons">
+            <ul className='home-about-social-links'>
+              <li className='social-icons'>
                 <a
-                  href="https://github.com/rhagueweb"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="icon-colour  home-social-icons"
+                  href='https://github.com/rhagueweb'
+                  target='_blank'
+                  rel='noreferrer'
+                  className='icon-colour  home-social-icons'
                 >
                   <AiFillGithub />
                 </a>
               </li>
-              <li className="social-icons">
+              <li className='social-icons'>
                 <a
-                  href="https://twitter.com/rhagueweb"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="icon-colour  home-social-icons"
+                  href='https://twitter.com/rhagueweb'
+                  target='_blank'
+                  rel='noreferrer'
+                  className='icon-colour  home-social-icons'
                 >
                   <FaXTwitter />
                 </a>
               </li>
-              <li className="social-icons">
+              <li className='social-icons'>
                 <a
-                  href="https://www.linkedin.com/in/rhagueweb"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="icon-colour  home-social-icons"
+                  href='https://www.linkedin.com/in/rhagueweb'
+                  target='_blank'
+                  rel='noreferrer'
+                  className='icon-colour  home-social-icons'
                 >
                   <FaLinkedinIn />
                 </a>
               </li>
-              <li className="social-icons">
-                <a
-                  href="https://www.instagram.com/rhague90"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="icon-colour home-social-icons"
-                >
-                  <AiFillInstagram />
-                </a>
-              </li>
             </ul>
             <p>
-              Feel free to <span className="purple">connect </span>with me
+              Feel free to <span className='purple'>connect </span>with me
             </p>
           </Col>
         </Row>
