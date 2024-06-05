b9e21c7988bbc3ce37383793802c50d7b04cb707
Richard Hague <rhagueweb@gmail.com>
2024-06-06 00:11:52 +0100

    Updated AboutCard wording

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -9,12 +9,16 @@ function AboutCard() {
         <blockquote className='blockquote mb-0'>
           <p style={{ textAlign: 'justify' }}>
             Hi Everyone, I am <span className='purple'>Richard Hague</span>{' '}
-            (33), from{' '}
-            <span className='purple'> Macclesfield, England (UK).</span>
+            (33), from
+            <span className='purple'>Macclesfield, England (UK).</span>
             <br />
-            I am currently employed as a Consultant @ Zentso.
+            I am currently employed as a Consultant at Zentso.
             <br />
-            Outside of my 9-5, I work as a freelance web developer.
+            Outside of my 9-5, I am passionate about website development and
+            love keeping up with the latest technologies.
+            <br />
+            I have a keen interest in the MERN stack, particularly with Next.js
+            and Tailwind CSS, to create beautiful and responsive websites.
             <br />
             I have completed a degree in Multimedia Technologies (BSc) at
             Sheffield Hallam University from 2009-12.
@@ -23,6 +27,7 @@ function AboutCard() {
             Apart from web development, here are some other activities that I
             love!
           </p>
+
           <ul>
             <li className='about-activity'>
               <ImPointRight /> Table Tennis
@@ -34,7 +39,7 @@ function AboutCard() {
               <ImPointRight /> Travelling
             </li>
             <li className='about-activity'>
-              <ImPointRight /> Watching Football
+              <ImPointRight /> Football
             </li>
             <li className='about-activity'>
               <ImPointRight /> Playing Guitar
@@ -42,7 +47,7 @@ function AboutCard() {
           </ul>
 
           <p style={{ color: 'rgb(28 166 143)' }}>
-            "Strive to build things that make a difference!"{' '}
+            "Strive to build things that make a difference...and look pretty!"{' '}
           </p>
           <footer className='blockquote-footer'>Rick</footer>
         </blockquote>
