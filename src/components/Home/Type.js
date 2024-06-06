0978151a7d64ad304987403344984b55a857fcf0
Richard Hague <rhagueweb@gmail.com>
2024-06-06 18:20:42 +0100

    MS Clarity install and CV update

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -1,18 +1,19 @@
-import React from "react";
-import Typewriter from "typewriter-effect";
+import React from 'react';
+import Typewriter from 'typewriter-effect';
 
 function Type() {
   return (
     <Typewriter
       options={{
         strings: [
-          "iMIS Consultant",
-          "Stargazer",
-          "MERN Stack Developer",
-          "Local Table Tennis Player",
-          "Web Developer",
-          "Manchester United Fan",
-          "Freelancer",         
+          'iMIS Consultant',
+          'Stargazer',
+          'MERN Stack Developer',
+          'Local Table Tennis Player',
+          'Web Developer',
+          'Manchester United Fan',
+          'Freelancer',
+          'Girl Dad x 4!',
         ],
         autoStart: true,
         loop: true,
